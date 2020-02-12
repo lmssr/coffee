596f62669b5819f3bf2a6f878907e351a8070d2e
Luc Mosser <luc.mosser86@orange.fr>
2020-02-12 23:16:53 +0100

    home page

---
+++ src/components/home/info.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from "gatsby";
+
+export default function Info() {
+    return (
+      <section className="py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-10 col-sm-8 mx-auto text-center">
+              <p className="lead text-muted mb-5">
+                bla bla bla
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {
-  FaGulp
-} from 'react-icons/fa'
+import BackgroundSection from "../components/globals/BackgroundSection"
+import Info from "../components/home/info";
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <BackgroundSection
+    img={data.img.childImageSharp.fluid}
+    title="Coffee Shop"
+    />
+    <Info />
   </Layout>
 )
+
+export const query = graphql`
+{
+  img: file(relativePath: {eq: "coffee-bg.jpg"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`;
 
 export default IndexPage
