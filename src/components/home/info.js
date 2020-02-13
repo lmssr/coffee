1990487115ee6191d1f1fa44515ea5b52019009f
Luc Mosser <luc.mosser86@orange.fr>
2020-02-13 14:42:48 +0100

    connected contentful

--- src/components/home/info.js
+++ src/components/home/info.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Link} from "gatsby";
+import Title from "../globals/title";
 
 export default function Info() {
     return (
       <section className="py-5">
         <div className="container">
+        <Title title="our story" />
           <div className="row">
             <div className="col-10 col-sm-8 mx-auto text-center">
               <p className="lead text-muted mb-5">
-                bla bla bla
+                Necem ut lenius Gallus ut modum lenius excedamus refert modum refert me huius modi necem quorum huius multa quod cadaveribus scrutabatur cadaveribus modum leo pastus ferociens modum ut necem modum Gallus pastus Gallus cadaveribus est nihilo pastus huius quae multa cadaveribus professione evitandum ferociens lenius singula refert cadaveribus necem huius.
               </p>
+              <Link to="/about/">
+                <button className="btn text-uppercase btn-yellow">about page</button>
+              </Link>
             </div>
           </div>
         </div>
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,19 +5,21 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import "./bootstrap.min.css"
-import "./layout.css"
+import "./bootstrap.min.css";
+import "./layout.css";
 
-import Navbar from "./globals/Navbar.js"
+import Navbar from "./globals/Navbar";
+import Footer from "./globals/footer";
 
 const Layout = ({ children }) =>
   <>
   <Navbar />
   {children}
+  <Footer />
   </>;
 
 
